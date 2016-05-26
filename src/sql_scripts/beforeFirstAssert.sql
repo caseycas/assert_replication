afc82fcf7881c1f44ac6fe67318417aae2b26069
Casey Casalnuovo <caseycas@Caseys-MBP.home>
2016-05-26 13:49:22 -0400

    Removing some sql scripts not currently used in the replication.

--- src/sql_scripts/beforeFirstAssert.sql
+++
@@ -1,6 +0,0 @@
-SELECT agg.project, agg.sha, agg.language, agg.file_name, agg.is_test, agg.method_name, agg.assertion_add,
-  agg.assertion_del, agg.total_add, agg.total_del, agg.is_bug, agg.author, agg.commit_date, date.first_assert_date
-  into assert_july_2015_no_merge1.fc_everything_before_1st_assert
-  FROM assert_july_2015_no_merge1.fc_everything_src_agg_fixed as agg INNER JOIN assert_july_2015_no_merge1.first_assert_date as date
-  ON agg.project = date.project AND agg.file_name = date.file_name AND agg.method_name = date.method_name 
-  where agg.commit_date <= date.first_assert_date;
